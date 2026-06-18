d0900966148ab56c1a196967a587869397115c6c
Stepan Shvets <shvetss2112@gmail.com>
2026-06-18 18:20:14 +0300

    cooler test

--- test.sh
+++ test.sh
@@ -1,3 +1,17 @@
-go  build -o bin  cmd/crypto/main.go 
+go  build -o bin  cmd/crypto/main.go
+
+echo Encrypt with chunk size 10
+./bin encrypt fence data/ducati.jpg data/cipher 10 50
+echo 
+echo Encrypt with chunk size 500
 ./bin encrypt fence data/ducati.jpg data/cipher 500 50
-./bin decrypt fence data/cipher data/check.jpg 500 50
+echo
+echo Encrypt with chunk size 10000
+./bin encrypt fence data/ducati.jpg data/cipher 10000 50
+echo 
+echo Encrypt with chunk size 6000000
+./bin encrypt fence data/ducati.jpg data/cipher_sequential 6000000 50
+
+echo
+echo Decrypt
+./bin decrypt fence data/cipher data/check.jpg 10000 50
